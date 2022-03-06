da68e582a53f2c41d82b43c896f8592f92d10248
xomien <xomien01@gmail.com>
2022-03-05 20:14:21 -0500

    Allow images

--- src/middleware.py
+++ src/middleware.py
@@ -3,5 +3,9 @@ from config import AUTH
 
 
 async def auth(request):
+    # allow images
+    if request.path.endswith(".png") or request.path.endswith(".jpg") or request.path.endswith(".jpeg"):
+        return None
+
     if request.headers.get("Authorization") != AUTH:
         return text("Unauthorized", status=401)
